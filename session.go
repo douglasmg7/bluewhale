4542e068749668c8cae4cef546f2a18d4f353c16
douglasmg7 <douglasmg7@gmail.com>
2019-02-14 20:36:48 -0200

    creating session data

--- session.go
+++ session.go
@@ -11,40 +11,50 @@ import (
   "time"
 )
 
+type session_data struct {
+  name string
+}
+
 // [session]user_id
-var userIdMap = map[string]string{}
+var userIdMap = map[string]int{}
+
+// [user_id]session_data
+var sessionDataMap = map[int]session_data{}
+
+func data(int) {
+
+}
 
 // get user from uuid session
 func UserId(req *http.Request) (int, error) {
   cookie, err := req.Cookie("session")
   // no cookie.
   if err == http.ErrNoCookie {
-    return nil, nil
+    return 0, nil
     // some error
   } else if err != nil {
-    return nil, err
+    return 0, err
   }
   // some
   if cookie != nil {
     session := cookie.String()
     userId := userIdMap[session]
-    if userId != nil {
+    if userId != 0 {
       return userId, nil
     } else {
       // get from db
       err = db.QueryRow("select user_id from session where uuid = ?", cookie.String()).Scan(&userId)
       if err != nil {
-        return nil, err
+        return 0, err
       }
-      if userId != "" {
+      if userId != 0 {
         userIdMap[session] = userId
         return userId, nil
       }
     }
-    // no cookie
-  } else {
-    return nil, nil
   }
+  // no cookie
+  return 0, nil
 }
 
 func NewSession(w http.ResponseWriter, userId int) error {
@@ -53,6 +63,7 @@ func NewSession(w http.ResponseWriter, userId int) error {
   if err != nil {
     return err
   }
+  sessinoString := session.String()
   // save cookie
   http.SetCookie(w, &http.Cookie{
     Name:  "session",
@@ -71,6 +82,6 @@ func NewSession(w http.ResponseWriter, userId int) error {
     return err
   }
   // set session cache
-  userIdMap[session] = userId
+  userIdMap[sessinoString] = userId
   return nil
 }
