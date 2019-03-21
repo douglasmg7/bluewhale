d17b996e0484ec216350253a75b06cd7ec2104e8
douglasmg7 <douglasmg7@gmail.com>
2019-03-21 17:53:44 -0300

    sub-menu transition working

--- static/js/layout.js
+++ static/js/layout.js
@@ -6,23 +6,55 @@
         menuLink = document.getElementById('menuLink'),
         content  = document.getElementById('main');
 
-    // Hide show sub-menus.
+    // Collapse/expand sub-items.
     let toggleItems = document.getElementsByClassName("toggle-items");
     for (let i = 0; i < toggleItems.length; i++) {
-        let items = toggleItems[i].nextElementSibling;
+        let element = toggleItems[i].nextElementSibling;
         toggleItems[i].onclick =  function(e){
-            // console.log(items);
-            // console.log(`display: ${items.style.display}`);
-            if (items.style.display === "none") {
-                items.style.display = "block"
+            if (element.getAttribute('data-expanded') === "true") 
+            {
+                collapseElement(element);
             } else {
-                items.style.display = "none"
+                ExpandElement(element);
             }
-            // items.style.display = "none"
         }
     }
-    console.log(toggleItems);
 
+    // Collapse element.
+    function collapseElement(element){
+        // Get the height of the element's inner content, regardless of its actual size.
+        let elementHeight = element.scrollHeight; 
+        // Temporarily disable all css transitions.
+        let elementTransition = element.style.transition; 
+        element.style.transition = ""
+        // On the next frame (as soon as the previous style change has taken effect),
+        // explicitly set the element's height to its current pixel height, so we aren't transitioning out of 'auto'.
+        requestAnimationFrame(function(){
+            element.style.height = elementHeight + "px";
+            element.style.transition = elementTransition;
+            // On the next frame (as soon as the previous style change has taken effect),
+            // have the element transition to height: 0.
+            requestAnimationFrame(function(){
+                element.style.height = "0" + "px";
+                element.setAttribute('data-expanded', "false");
+            });
+        });
+    }
+    // Expand element.
+    function ExpandElement(element){
+        // Get the height of the element's inner content, regardless of its actual size.
+        let elementHeight = element.scrollHeight; 
+        // Have the element transition to the height of its inner content.
+        element.style.height = elementHeight + "px";
+        // When the next css transition finishes (which should be the one we just triggered).
+        element.addEventListener('transitionend', function(e){
+            // Remove this event listener so it only gets triggered once.
+            element.removeEventListener('transitionend', arguments.callee);
+            // Remove "height" from the element's inline styles, so it can return to its initial value.
+            // element.style.height = null;
+        });
+        element.setAttribute('data-expanded', "true");
+    }
     // Toggle class.
     function toggleClass(element, className) {
         var classes = element.className.split(/\s+/),
